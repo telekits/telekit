37930ea110fb298cae4d7dc883a0b113592a0e59
Denis Maslennikov <nof1000@mail.ru>
2017-03-18 12:41:54 +0300

    Reloaded of Telekit 🤓 All functionality from `middleware` and `helper` migrated to `basic` for minimization and simplicity. Also, now `index` is only for exports. Its implementation removed but partly recreated in `telekit`.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /**
- * The powerful, but an elegantly simple toolkit for making Telegram Bots
+ * The strong and elegant of Telegram Bot Framework;
+ * This module to provide a collection one to one.
  *
  * @module telekit
  * @author Denis Maslennikov <mrdenniska@gmail.com> (nofach.com)
@@ -7,88 +8,11 @@
  */
 
 /** Dependencies */
-const { Middleware, Context, Client, Helper } = require('./lib/');
-const { EventEmitter } = require('events');
+const telekit = require('./lib/telekit.js');
 const teleapi = require('teleapi');
-const debug = require('debug')('telekit');
-
-/**
- * Implementation
- * @public
- */
-class Kit extends EventEmitter {
-    constructor(options = {}) {
-        super();
-
-        this.options = options;
-
-        if (!options.token) {
-            throw new Error('Token is missing');
-        }
-
-        /** Create instance of teleapi */
-        this.api = teleapi(options.token);
-
-        /** Create middleware and bind it with kit */
-        this.middleware = new Middleware(this);
-        this.use = this.middleware.use.bind(this.middleware);
-
-        /** Create helper and set listeners */
-        this.helper = new Helper();
-        this.helper.on('update', this._update.bind(this));
-
-        /** Create global context */
-        this.context = Context(this);
-
-        /** Create polling or webhook client */
-        this.client = Client(this.api, this.options);
-
-        if (!this.client) {
-            debug('added insertUpdate because instance without client');
-            this.insertUpdate = (update) => {
-                this.helper.put(update);
-            };
-        } else {
-            debug('client is connected.');
-            this.client.update = (update) => {
-                this.helper.put(update);
-            };
-        }
-    }
-
-    /** @private */
-    _update(update, info) {
-        /** Update context */
-        this.context.update = update[info.original];
-
-        /** Emitted `beforeUpdate` event with context and update info */
-        this.emit('beforeUpdate', this.context, info);
-
-        /** Emitted `update` event with context and update name */
-        this.update(this.context, info.name);
-        this.emit('update', this.context, info.name);
-        this.middleware.transmit('update', this.context, info.name).then(() => {
-            /** Emitted name of update with context */
-            this[info.name](this.context);
-            this.emit(info.name, this.context);
-            return this.middleware.transmit(info.name, this.context).then(() => {
-                /** Emitted afterUpdate event with context and update info */
-                this.emit('afterUpdate', this.context, info);
-            });
-        }).catch((error) => {
-            error();
-        });
-    }
-
-    update(context, type) { /** Abstract method */ }
-    message(context) { /** Abstract method */ }
-    post(context) { /** Abstract method */ }
-    inline(context) { /** Abstract method */ }
-    callback(context) { /** Abstract method */ }
-}
 
 /** Exports */
-module.exports = Object.assign((options) => {
-    if (options) return new Kit(options);
-    return Kit;
-}, { api: teleapi, kit: Kit });
+module.exports = Object.assign(function Telekit(options) {
+    if (options) return new telekit(options);
+    return telekit;
+}, { api: teleapi });
