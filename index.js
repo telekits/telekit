878900cc29f5ff5ab9c2f15346c4fb5b3c756747
Denis Maslennikov <mrdenniska@gmail.com>
2017-05-25 18:26:18 +0300

    Migration, and style fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
- * The strong and elegant of Telegram Bot Framework;
- * This module to provide a collection one to one.
+ * The strong and elegant of the Telegram Bot Framework;
+ * This module to provide an export function as is an object.
  *
  * @module telekit
  * @author Denis Maslennikov <mrdenniska@gmail.com> (nofach.com)
@@ -8,11 +8,11 @@
  */
 
 /** Dependencies */
-const Telekit = require('./lib/telekit.js');
-const teleapi = require('teleapi');
+const API = require('teleapi');
+const Kit = require('./lib');
 
 /** Exports */
 module.exports = Object.assign((options) => {
-    if (options) return new Telekit(options);
-    return Telekit;
-}, { api: teleapi });
+    if (options) return new Kit(options);
+    return Kit;
+}, { api: API });
